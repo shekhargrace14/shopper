999306e717c57a1fe246593c959bd5d5ee7f3eb2
shekhargrace14 <shekhargrace14@gmail.com>
2024-07-04 19:00:22 +0530

    changes

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -18,20 +18,20 @@ const SearchResult = ({ searchInput }) => {
 
   return (
     <div>{searchInput===""? " " : 
-      <main className='absolute border-2 rounded-2xl bg-white z-30'>
+      <main className=' w-[80%] max-h-[75vh] overflow-y-scroll absolute border-2 rounded-2xl bg-white z-30'>
         {searchResults.map((product, index) => (
           <Link key={index} to={`/singleproduct/${product.id}`}>
-            <div className="row flex justify-between items-center gap-4 hover:bg-gray-100 p-4">
+            <div className="row flex justify-between items-center gap-4 hover:bg-gray-100 ">
               <figure className="column w-[30%] flex justify-center items-center">
-                <img className="sm:w-[50%] md:w-[50%]" src={product.productImage} alt={product.title} />
+                <img className="sm:w-[20%] md:w-[20%]" src={product.productImage} alt={product.title} />
               </figure>
-              <h1 className="font-primary-font sm:text-4xl text-3xl font-bold sm:my-6 my-4">{product.title}</h1>
+              <h1 className="font-primary-font sm:text-1xl text-1xl font-bold sm:my-2 my-2">{product.title}</h1>
               <div className="column grid grid-cols-2 items-center">
-                <p className='font-primary-font sm:text-4xl text-2xl text-primary-color font-semibold'>${product.price}</p>
+                <p className='font-primary-font sm:text-1xl text-base text-primary-color font-semibold'>${product.price}</p>
                 <div className="price flex flex-wrap items-center justify-start gap-4 sm:my-6 my-2">
                   <div className="flex gap-4">
-                    <p className='text-primary-font text-accent font-bold text-xs'>15% Off</p>
-                    <del className='text-gray-500 text-1xl font-bold'>${product.price}</del>
+                    {/* <p className='text-primary-font text-accent font-bold text-xs'>15% Off</p> */}
+                    {/* <del className='text-gray-500 text-1xl font-bold'>${product.price}</del> */}
                   </div>
                 </div>
               </div>
